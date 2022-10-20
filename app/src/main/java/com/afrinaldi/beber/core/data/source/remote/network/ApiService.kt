6357904188231ebff07923888769fe5a98edf444
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2022-10-20 22:03:59 +0700

    add ApiService

--- app/src/main/java/com/afrinaldi/beber/core/data/source/remote/network/ApiService.kt
+++ app/src/main/java/com/afrinaldi/beber/core/data/source/remote/network/ApiService.kt
@@ -1,5 +1,55 @@
 package com.afrinaldi.beber.core.data.source.remote.network
 
-interface ApiService {
+import com.afrinaldi.beber.core.data.source.remote.response.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface ApiService {
+    @GET("top-headlines")
+    suspend fun getHeadlines(
+        @Query("country") country: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getSports(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getTechnology(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getBusiness(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getHealth(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getEntertainment(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
+
+    @GET("everything")
+    suspend fun getNewsSearch(
+        @Query("q") keyword: String?,
+        @Query("language") language: String?,
+        @Query("apiKey") apiKey: String?
+    ): NewsResponse
 }

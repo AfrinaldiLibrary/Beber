7d61eebf1808cb467af458688dc462a404492018
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2022-12-18 21:10:01 +0700

    rollback

--- core/src/main/java/com/afrinaldi/core/di/DatabaseModule.kt
+++ core/src/main/java/com/afrinaldi/core/di/DatabaseModule.kt
@@ -2,16 +2,22 @@ package com.afrinaldi.core.di
 
 import androidx.room.Room
 import com.afrinaldi.core.data.source.local.room.NewsDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
     factory { get<NewsDatabase>().newsDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             NewsDatabase::class.java,
             "News.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
